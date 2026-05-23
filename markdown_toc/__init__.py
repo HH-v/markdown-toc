b547dcc13d37c0cbbfe24aedac40be94ebb1b11d
HH-v <moliqingchaplus@gmail.com>
2026-05-23 22:50:12 +0800

    chore: bump version to 0.2.0

--- markdown_toc/__init__.py
+++ markdown_toc/__init__.py
@@ -1,5 +1,7 @@
-from .generator import generate_toc
+from .generator import generate_toc, insert_toc
 from .parser import parse_headings
+from .writer import write_toc_file
+from .cli import main
 
-__version__ = "0.1.0"
-__all__ = ["generate_toc", "parse_headings"]
+__version__ = "0.2.0"
+__all__ = ["generate_toc", "insert_toc", "parse_headings", "write_toc_file", "main"]
